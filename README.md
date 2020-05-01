018544a60f2d9b06a8a906ec9d21aef57418b21d
root <root@rodrigo-ubuntu-newfileserver.novalocal>
2020-04-30 22:38:48 -0300

    versao 1.1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # proxyfinder
-Programa que baixa lista de proxy do site https://www.proxy-list.download/ e testa todos eles.
+Programa que baixa lista de proxy do site https://www.proxy-list.download/ , testa cada um dos proxy fazendo acesso a uma página verificando se teve sucesso. 
+No final ele lista apenas os proxys que estão funcionando e em ordem de delay. 
--- proxy_finder.py
+++ proxy_finder.py
@@ -1,62 +1,52 @@
 #!/usr/bin/env python3
-import urllib.request
+from concurrent.futures import ThreadPoolExecutor, wait, as_completed
 import urllib.request
 import urllib.parse
 import requests
 import json
-import _thread
-import concurrent.futures
 import os
 import sys
 from pprint import pprint
-from collections import namedtuple
 import time
-from types import SimpleNamespace as Namespace
-from tabulate import tabulate
 
 def truncate(number, decimals=0):
     multiplier = 10 ** decimals
     return int(number * multiplier) / multiplier
     
-class MeuLog:
-    __instance = None
-    @staticmethod
-    def getInstance(filename):
-        if MeuLog.__instance == None:
-            MeuLog(filename)
-        return MeuLog.__instance
-    def __init__(self,filename):
-        self.f=''
-        if MeuLog.__instance != None:
-            raise Exception("This class is a singleton!")
-        else:
-            if not self.f: 
-                self.f = open(filename, 'w+')
-            MeuLog.__instance = self
-    def write(self,string):
-        self.f.write(string)
-    def __self__(self):
-        self.f.close()
+class Proxy:
+    def __init__(self,ip,port,anon,country,iso):
+        self.ip=ip
+        self.port=str(port)
+        self.anon=anon
+        self.country=country
+        self.iso=iso
+        self.valid=''        
+        self.delay=''
+        self.ipport=f'{self.ip}:{self.port}'
         
-def proxy_test(ip,port,proxylist):
-    proxy_address = 'http://' + ip + ':' + str(port)
-    proxies = { 'http': proxy_address, 'https': proxy_address}    
+    def __repr__(self):
+        return('%-22s %-15s %-10.2s %-5s \n' % (
+                self.ipport, self.country, self.delay, self.valid))
+        
+        
+def proxy_validate(proxy):
+    proxy_address = 'http://' + proxy.ipport
+    proxies = { 'http': proxy_address, 'https': proxy_address }    
     try:
-        start = time.time()        
-        r = requests.get('https://ip.seeip.org/jsonip?', proxies=proxies, timeout=10)        
+        start = time.time()
+        r = requests.get('https://ip.seeip.org/jsonip?', proxies=proxies, timeout=30)     
         response = r.text.strip()
         end = time.time()
         delay = truncate((end-start),2)
         if 'ip' in response:
-            print(f'{ip}:{port}\t{delay}')
-            proxylist.append(f'{ip}:{port}     {delay}ms')
-            return True
-        else:
-            return False
-    except:       
-        #print( e = sys.exc_info()[0])
-        # print('Erro')
-        return False
+            #print(f'Proxy {proxy.ipport} OK.')
+            proxy.valid=True
+            proxy.delay=delay
+    except:              
+        proxy.valid=False
+        pass
+        # print( e = sys.exc_info()[0])
+        #print(f'Proxy {proxy.ipport} not working.')
 
 def get_proxy_list():
     try: 
@@ -73,42 +63,66 @@ def get_proxy_list():
         return LISTA
     except: 
         print('Não foi possível baixar lista de proxies...')
-        exit
-        return None
-
-
+        exit()
 #######################################
 #  MAIN
 #######################################
 
 if __name__ == '__main__':
    
+    # Busca lista de proxy da internet
     LISTA = get_proxy_list()  
-    print('------------------------')
-    print(f'Testando proxies...')  
-    print('------------------------')
+
+    # CARREGA LISTA DE PROXIES
+    proxylist = list()
+    for server in LISTA:
+        proxy = Proxy(
+            server.get('IP'),
+            server.get('PORT'),
+            server.get('ANON'),
+            server.get('COUNTRY'),
+            server.get('ISO'))
+        proxylist.append(proxy)
         
-    with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
-        proxylist = list()
-        for server in LISTA:
-            IP      = server.get('IP')
-            PORT    = server.get('PORT')
-            ANON    = server.get('ANON') 
-            COUNTRY = server.get('COUNTRY') 
-            ISO     = server.get('ISO')
-            TYPE    = server.get('TYPE')
-            executor.submit(proxy_test,IP,PORT,proxylist)
-    # Aguarda terminar os processos
-    executor.shutdown(wait=True)
+    # TESTA OS PROXIES
+    print('------------------------')
+    print(f'Testando proxies...    ')  
+    print('------------------------')    
     
-    print('---------------------------------')
-    print(f' TOTAL DE PROXIES= {len(LISTA)}')
-    print(f' PROXIES OK= {len(proxylist)}')
-    with open('proxylist.txt', mode='wt', encoding='utf-8') as myfile:
-        myfile.write('\n'.join(proxylist))
+    futures = []
+#    with ThreadPoolExecutor(max_workers=10) as pool:
+#        for proxy in proxylist[0:10]:    
+#            future.append(pool.submit(proxy_validate,proxy))
+    
+    pool = ThreadPoolExecutor(max_workers=20)
+    for proxy in proxylist:
+        futures.append(pool.submit(proxy_validate,proxy))
+        
+    completados=0
+    for x in as_completed(futures):
+        completados+=1
+        total=len(futures)
+        print('Completado: %3d %% \r' % (completados/total*100),end='')    
+    print('------------------------------------------------------------------')  
+    
+    # Mostra todos proxy organizado por delay
+    proxyvalid = [p for p in proxylist if p.valid == True]
+    sortedbydelay = sorted(proxyvalid, key=lambda proxy: proxy.delay)  
+    #print(sortedbydelay)
+    
+    for proxy in sortedbydelay:
+        print('%-22s %-15s %6.2f ms   %-5s' % 
+             (proxy.ipport, proxy.country, proxy.delay, proxy.anon))
+    
+    print('------------------------------------------------------------------')  
+    print(f' TOTAL DE PROXIES:   {len(proxylist)}')
+    print(f' PROXIES FUNCONANDO: {len(proxyvalid)}')
+    
+    file = open('proxylist.txt','w+')
+    for proxy in sortedbydelay:
+        file.write(f'{proxy.ipport} \n')    
+    file.close()
+    
     print('---------------------------------')
     print('  ARQUIVO SALVO: proxylist.txt   ')
     print('---------------------------------')
-
-    
-
